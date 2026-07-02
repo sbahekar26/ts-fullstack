cb7c6acdc4405fd1cd4b051cec7c0b0b6c8beb3f
Siddharth Bahekar <siddharthbahekar13@gmail.com>
2026-07-02 03:48:41 -0400

    Add warmup: sum and even number filter

---
+++ warmup.js
@@ -0,0 +1,7 @@
+const nums = [4, 8, 15, 16, 23, 42];
+const even = nums.filter((n) => n % 2 === 0);
+console.log("Even numbers:", even);
+
+const nums1 = [4, 8, 15, 16, 23, 42];
+const total = nums1.reduce((sum, n) => sum + n, 0);
+console.log("Total:", total);
